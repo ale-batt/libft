c1a219b3cef56373361e85055cb29f91fc8a0f67
Melvin MOUSTAID <mmoustai@e2r11p18.42.fr>
2016-03-03 10:58:40 +0100

    [*] *.h

--- includes/struct.h
+++ includes/struct.h
@@ -6,9 +6,10 @@
 /*   By: world42 <world42@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/27 03:15:23 by world42           #+#    #+#             */
-/*   Updated: 2014/04/27 03:17:10 by world42          ###   ########.fr       */
+/*   Updated: 2016/03/03 10:38:21 by mmoustai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef STRUCT_H
 # define STRUCT_H
 
@@ -16,9 +17,9 @@
 
 typedef struct		s_list
 {
-    void          *content;
-    size_t        content_size;
-    struct s_list	*next;
-}                 t_list;
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
 
 #endif
