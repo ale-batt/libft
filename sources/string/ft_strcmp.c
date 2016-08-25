42c21f3d74a1d5b0259db87c796498e6268fe27a
Albert Le Batteux <albert.lebatteux@ekimetrics.com>
2016-08-25 14:22:37 +0200

    Change file as hierarchy

---
+++ sources/string/ft_strcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: world42 <world42@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/20 17:32:21 by world42           #+#    #+#             */
+/*   Updated: 2013/11/27 17:08:47 by world42          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned char	*s1_tmp;
+	unsigned char	*s2_tmp;
+
+	s1_tmp = (unsigned char*)s1;
+	s2_tmp = (unsigned char*)s2;
+	while (*s1_tmp == *s2_tmp)
+	{
+		if (!*s1_tmp && !*s2_tmp)
+			return (0);
+		s1_tmp++;
+		s2_tmp++;
+	}
+	return (*s1_tmp - *s2_tmp);
+}
