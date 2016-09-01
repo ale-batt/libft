eaf04f882f231fbbdc614b76fbb3882164e408d2
Albert Le Batteux <albert.lebatteux@ekimetrics.com>
2016-09-01 16:37:42 +0200

    Add putstrcolor in .h

--- includes/puts.h
+++ includes/puts.h
@@ -6,7 +6,7 @@
 /*   By: world42 <world42@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/26 13:59:30 by world42           #+#    #+#             */
-/*   Updated: 2016/09/01 16:34:15 by ale-batt         ###   ########.fr       */
+/*   Updated: 2016/09/01 16:37:25 by ale-batt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	ft_putbase(long l, int base, char *letters);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putchar(char c);
 void	ft_putdouble(double d, double epsilone);
-void	ft_putendl_color(char *str, char *color);
+void	ft_putstr_color(char *str, char *color);
 void	ft_putendl_fd(const char *s, int fd);
 void	ft_putendl(const char *s);
 void	ft_putfloat(float d, double epsilone);
