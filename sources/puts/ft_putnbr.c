402f0961d5d80b52ba465e1039f2ce6559b84288
Albert LE-BATTEUX <ale-batt@e2r12p18.42.fr>
2016-10-31 15:35:05 +0100

    add putunbr

--- sources/puts/ft_putnbr.c
+++ sources/puts/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: world42 <world42@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/27 03:44:47 by world42           #+#    #+#             */
-/*   Updated: 2014/04/27 03:44:48 by world42          ###   ########.fr       */
+/*   Updated: 2016/10/26 17:50:38 by ale-batt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,17 @@ void	ft_putnbr(int n)
 	}
 	else
 		ft_putchar(n + '0');
+}
+
+void	ft_putunbr(unsigned long long nb)
+{
+	if (nb == ULLONG_MAX)
+		ft_putstr("18446744073709551615");
+	else if (nb >= 10)
+	{
+		ft_putunbr(nb / 10);
+		ft_putunbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
 }
