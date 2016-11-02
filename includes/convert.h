839b503226866428e4187ac14d03f79f1a20b8e9
Albert LE-BATTEUX <ale-batt@e2r12p18.42.fr>
2016-11-02 16:30:15 +0100

    add ft_tohex

--- includes/convert.h
+++ includes/convert.h
@@ -6,7 +6,7 @@
 /*   By: world42 <world42@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/26 13:58:54 by world42           #+#    #+#             */
-/*   Updated: 2016/10/26 18:36:55 by ale-batt         ###   ########.fr       */
+/*   Updated: 2016/10/31 18:48:09 by ale-batt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 int			ft_atoi(const char *str);
 float		ft_atof(const char *str);
 char		*ft_itoa(int n);
+char		*ft_tohexa(unsigned long n);
 
 #endif
